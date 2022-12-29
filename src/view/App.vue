f2713ecd1300c0278e92f04749568eddd7fc1f86
Joe <soboleskijoe@gmail.com>
2022-12-28 19:25:21 -0800

    zoom and reorganization

--- src/view/App.vue
+++ src/view/App.vue
@@ -1,6 +1,6 @@
 <!-- eslint-disable vue/html-self-closing -->
 <template>
-  <div class="bg-zinc-900">
+  <div class="bg-zinc-900 overflow-hidden">
     <svg
       width="600"
       height="600"
@@ -18,38 +18,81 @@ const getRandomInt = (max) => {
 	return Math.floor(Math.random() * max)
 }
 
+const colorScale = ['orange', 'lightblue', '#B19CD9']
+// Node Dataset
+let nodes = [
+	{ name: '1', radius: Math.random() * 15 + 10, category: getRandomInt(3) },
+	{ name: '2', radius: Math.random() * 15 + 10, category: getRandomInt(3) },
+	{ name: '3', radius: Math.random() * 15 + 10, category: getRandomInt(3) },
+	{ name: '4', radius: Math.random() * 15 + 10, category: getRandomInt(3) },
+	{ name: '5', radius: Math.random() * 15 + 10, category: getRandomInt(3) },
+	{ name: '6', radius: Math.random() * 15 + 10, category: getRandomInt(3) },
+	{ name: '7', radius: Math.random() * 15 + 10, category: getRandomInt(3) },
+	{ name: '8', radius: Math.random() * 15 + 10, category: getRandomInt(3) },
+	{ name: '9', radius: Math.random() * 15 + 10, category: getRandomInt(3) }
+]
+// Side Dataset
+let edges = [
+	{ source: getRandomInt(nodes.length), target: getRandomInt(nodes.length) },
+	{ source: getRandomInt(nodes.length), target: getRandomInt(nodes.length) },
+	{ source: getRandomInt(nodes.length), target: getRandomInt(nodes.length) },
+	{ source: getRandomInt(nodes.length), target: getRandomInt(nodes.length) },
+	{ source: getRandomInt(nodes.length), target: getRandomInt(nodes.length) },
+	{ source: getRandomInt(nodes.length), target: getRandomInt(nodes.length) },
+	{ source: getRandomInt(nodes.length), target: getRandomInt(nodes.length) },
+	{ source: getRandomInt(nodes.length), target: getRandomInt(nodes.length) },
+	{ source: getRandomInt(nodes.length), target: getRandomInt(nodes.length) },
+	{ source: getRandomInt(nodes.length), target: getRandomInt(nodes.length) }
+]
+
+// Create a new force guide diagram
+let forceSimulation = d3.forceSimulation()
+	.force('link', d3.forceLink())
+	.force('charge', d3.forceManyBody().strength(-1000))
+	.force('center', d3.forceCenter())
+	.force('collision', d3.forceCollide().radius((d) => { return d.radius }))
+
+// drag
+const drag = (simulation) => {
+	const dragstarted = (event) => {
+		if (!event.active) simulation.alphaTarget(0.3).restart()
+		event.subject.fx = event.subject.x
+		event.subject.fy = event.subject.y
+	}
+
+	const dragged = (event) => {
+		event.subject.fx = event.x
+		event.subject.fy = event.y
+	}
+
+	const dragended = (event) => {
+		if (!event.active) simulation.alphaTarget(0)
+		event.subject.fx = null
+		event.subject.fy = null
+	}
+
+	return d3.drag()
+		.on('start', dragstarted)
+		.on('drag', dragged)
+		.on('end', dragended)
+}
+
 onMounted(() => {
 	let svg = d3.select('svg')
 	let width = svg.attr('width')
 	let height = svg.attr('height')
 	let g = svg.append('g')
 
-	const colorScale = ['orange', 'lightblue', '#B19CD9']
-	// Node Dataset
-	let nodes = [
-		{ name: '1', radius: Math.random() * 15 + 10, category: getRandomInt(3) },
-		{ name: '2', radius: Math.random() * 15 + 10, category: getRandomInt(3) },
-		{ name: '3', radius: Math.random() * 15 + 10, category: getRandomInt(3) },
-		{ name: '4', radius: Math.random() * 15 + 10, category: getRandomInt(3) },
-		{ name: '5', radius: Math.random() * 15 + 10, category: getRandomInt(3) },
-		{ name: '6', radius: Math.random() * 15 + 10, category: getRandomInt(3) },
-		{ name: '7', radius: Math.random() * 15 + 10, category: getRandomInt(3) },
-		{ name: '8', radius: Math.random() * 15 + 10, category: getRandomInt(3) },
-		{ name: '9', radius: Math.random() * 15 + 10, category: getRandomInt(3) }
-	]
-	// Side Dataset
-	let edges = [
-		{ source: getRandomInt(nodes.length), target: getRandomInt(nodes.length) },
-		{ source: getRandomInt(nodes.length), target: getRandomInt(nodes.length) },
-		{ source: getRandomInt(nodes.length), target: getRandomInt(nodes.length) },
-		{ source: getRandomInt(nodes.length), target: getRandomInt(nodes.length) },
-		{ source: getRandomInt(nodes.length), target: getRandomInt(nodes.length) },
-		{ source: getRandomInt(nodes.length), target: getRandomInt(nodes.length) },
-		{ source: getRandomInt(nodes.length), target: getRandomInt(nodes.length) },
-		{ source: getRandomInt(nodes.length), target: getRandomInt(nodes.length) },
-		{ source: getRandomInt(nodes.length), target: getRandomInt(nodes.length) },
-		{ source: getRandomInt(nodes.length), target: getRandomInt(nodes.length) }
-	]
+	// Zoom
+	const handleZoom = (event) => {
+		g.attr('transform', event.transform)
+	}
+
+	let zoom = d3.zoom()
+		.scaleExtent([0.1, 10])
+		.on('zoom', handleZoom)
+
+	svg.call(zoom)
 
 	// Draw side
 	let links = g.append('g')
@@ -71,13 +114,6 @@ onMounted(() => {
 			.attr('transform', (d) => { return `translate(${d.x}, ${d.y})` })
 	}
 
-	// Create a new force guide diagram
-	let forceSimulation = d3.forceSimulation()
-		.force('link', d3.forceLink())
-		.force('charge', d3.forceManyBody().strength(-1000))
-		.force('center', d3.forceCenter())
-		.force('collision', d3.forceCollide().radius((d) => { return d.radius }))
-
 	// Generate node data
 	forceSimulation.nodes(nodes)
 		.on('tick', ticked)
@@ -92,39 +128,11 @@ onMounted(() => {
 		.x(width / 2)
 		.y(height / 2)
 
-	// drag
-	const drag = (simulation) => {
-		const dragstarted = (event) => {
-			if (!event.active) simulation.alphaTarget(0.3).restart()
-			event.subject.fx = event.subject.x
-			event.subject.fy = event.subject.y
-		}
-
-		const dragged = (event) => {
-			event.subject.fx = event.x
-			event.subject.fy = event.y
-		}
-
-		const dragended = (event) => {
-			if (!event.active) simulation.alphaTarget(0)
-			event.subject.fx = null
-			event.subject.fy = null
-		}
-
-		return d3.drag()
-			.on('start', dragstarted)
-			.on('drag', dragged)
-			.on('end', dragended)
-	}
-
 	// Create group
 	let gs = g.selectAll('.circleText')
 		.data(nodes)
 		.enter()
 		.append('g')
-		.attr('transform', (d) => {
-			return `translate(${d.x}, ${d.y})`
-		})
 		.attr('class', 'fill-white')
 		.call(drag(forceSimulation))
 
