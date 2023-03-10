d44405409af4804c93813b12e05b45006bde935e
joesobo <soboleskijoe@gmail.com>
2023-03-10 14:53:06 -0800

    directory connections working

--- src/utils/fetchDirectoryConnections.ts
+++ src/utils/fetchDirectoryConnections.ts
@@ -3,6 +3,7 @@ import type { Connection, Directory, File } from './types'
 export const fetchDirectoryConnections = (files: File[], dirs: Directory[]) => {
 	const connections: Connection[] = []
 
+	// Find connections between files and directories
 	files.forEach((file, fileIndex) => {
 		const filePathArray = file.name.split('/')
 		filePathArray.pop()
@@ -11,9 +12,22 @@ export const fetchDirectoryConnections = (files: File[], dirs: Directory[]) => {
 		dirs.forEach((dir, dirIndex) => {
 			if (fileDirectory === dir.name) {
 				connections.push({
-					id: `${fileIndex}-${dirIndex}`,
-					source: fileIndex,
-					target: dirIndex
+					id: `${dirIndex}-${fileIndex+dirs.length}`,
+					source: dirIndex,
+					target: fileIndex+dirs.length
+				})
+			}
+		})
+	})
+
+	// Find connections between directories
+	dirs.forEach((dir, dirIndex) => {
+		dirs.forEach((testDir, testDirIndex) => {
+			if (dir.name.includes(testDir.name) && dir.name !== testDir.name) {
+				connections.push({
+					id: `${dirIndex}-${testDirIndex}`,
+					source: dirIndex,
+					target: testDirIndex
 				})
 			}
 		})
--- src/utils/messenger.ts
+++ src/utils/messenger.ts
@@ -35,6 +35,7 @@ const receiveMessages = async (webview: vscode.Webview) => {
 			} else {
 				connections = fetchDirectoryConnections(files, dirs)
 				nodes = processData(files, nodeSize, connections, dirs)
+				// console.log('TEST', connections, nodes)
 			}
 
 			await webview.postMessage({
