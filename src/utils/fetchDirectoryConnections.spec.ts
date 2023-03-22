edac4fbd70b3520af2c2a52d9c03715825eb316f
joesobo <soboleskijoe@gmail.com>
2023-03-22 12:05:10 -0700

    circleci pnpm

--- src/utils/fetchDirectoryConnections.spec.ts
+++ src/utils/fetchDirectoryConnections.spec.ts
@@ -12,15 +12,13 @@ describe('fetchDirectoryConnections', () => {
     const files: File[] = [
       { name: 'test/test.js', lines: 0 },
       { name: 'test/test2.js', lines: 1 },
-			{ name: 'abc/test3.js', lines: 2 },
+      { name: 'abc/test3.js', lines: 2 },
     ]
     const expectedConnections: Connection[] = [
       { id: '0-2', source: 0, target: 2 },
       { id: '0-3', source: 0, target: 3 },
       { id: '1-4', source: 1, target: 4 },
     ]
-
-    console.log(fetchDirectoryConnections(files, dirs))
 
     expect(fetchDirectoryConnections(files, dirs)).toEqual(expectedConnections)
   })
